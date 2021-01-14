8301ca84a064cf013a3678de6c5330878e517b7f
Dmitry <naumugin@gmail.com>
2021-01-14 16:50:16 +0700

    Я не бог фронтэнда version2 it works now again

--- js/vue-test.js
+++ js/vue-test.js
@@ -1,9 +1,18 @@
 const HelloVueApp = {
 	data() {
 		return {
-			message: 'Hello Vue!!'
+			message: 'Hello Vue!!',
+
+		}
+	}
+}
+const Count = {
+	data() {
+		return {
+			counter: 0
 		}
 	}
 }
 
 Vue.createApp(HelloVueApp).mount('#hello-vue')
+Vue.createApp(Count).mount('#count')
